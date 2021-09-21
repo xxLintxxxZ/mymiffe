2e7850c9482d214b3c93d4a993b5b0ad313a72de
linxxnint <vasteple@gmail.com>
2021-09-21 22:53:59 +0800

    added tabulate amount spent and the cash left in hand in count.js

--- count.js
+++ count.js
@@ -1,24 +1,21 @@
 //LOGIC CHECKING //
-//qtyWanted x rate < $30
+//qtyWanted x rate < $30; if any player bid more than cashHeld, should "catch"
 //* name, qty and rate to test
-//==============================//
-//===========DEMAND PHASE======//
-//============================//
+//===============================//
+//===========DEMAND PHASE========//
+//===============================//
   const playerInfo = [
-    {name: "don", qty: 8, rate: 4 , total: 0, qtyBought: 0},
-    {name: "jane", qty: 5, rate: 2, total: 0, qtyBought: 0},
-    {name: "dean", qty: 6, rate: 8, total: 0, qtyBought: 0},
+    {name: "don", qty: 2, rate: 6 , total: 0, cashHeld: 30},
+    {name: "jane", qty: 5, rate: 2, total: 0, cashHeld: 30},
+    {name: "dean", qty: 6, rate: 5, total: 0, cashHeld: 30},
     {demand: ''}
   ]
+
 
   playerInfo[3].demand = playerInfo[0].qty + playerInfo[1].qty + playerInfo[2].qty;
   let d = playerInfo[0].qty + playerInfo[1].qty + playerInfo[2].qty;
   console.log(playerInfo) //test
 
-  //this is misleading // 
-  playerInfo[0].total = playerInfo[0].qty * playerInfo[0].rate
-  playerInfo[1].total = playerInfo[1].qty * playerInfo[1].rate
-  playerInfo[2].total = playerInfo[2].qty * playerInfo[2].rate
   //console.log(playerInfo)  // checking to see array info 
 
 // program to extract value as an array from an array of objects
@@ -78,49 +75,36 @@ let last = left2 -p1qtyBought
 if (playerInfo[2].qty > totalAvail) {
   console.log("Insufficient t-shirts!")
 }
-console.log( playerInfo[2].name + ' has bought ' + p3qtyBought + " t-shirts") // Player2 has made the highest offer so he gets the qty
+console.log( playerInfo[2].name + ' has bought ' + p3qtyBought + " t-shirts") // Player has made the highest offer so he gets the qty
 console.log("Remaining t-shirts after " + playerInfo[2].name +  " has bought : "+ left1)// left 1 are the remaining shirts for the 2 players
 
 if (left1 > combinedQtyP1P2) {
     console.log("Insufficient t-shirts!")}
     console.log( playerInfo[1].name + ' has bought ' + p2qtyBought + " t-shirts")
-    console.log("Remaining tshirts after " + playerInfo[1].name +  " has bought : "+ left2) //7
+    console.log("Remaining t-shirts after " + playerInfo[1].name +  " has bought : "+ left2) //7
     
 if (left2 > playerInfo[0].qty) {
   console.log("Insufficient t-shirts!")
 }
     console.log( playerInfo[0].name + ' has bought ' + p1qtyBought + " t-shirts")
-    console.log("Remaining tshirts after " + playerInfo[0].name +  " has bought : "+ last) 
-
-// program to extract value as an array from an array of objects
-// /***********          QTY SORT ************************************/
-// /* below function is sorting the qty of the playerInfo array */
-// const qtySort = playerInfo.sort(function(a, b){return a.qty - b.qty});
-
-// console.log(qtySort) // from smallest to largest
-//console.log(playerInfo) //array has been sorted changed from smallest qty to largest
-
-// const qtyMax = playerInfo[2].qty  // 7
-// const qtyMedian = playerInfo[1].qty // 6
-// const qtyMin = playerInfo[0].qty //5
-
-// console.log("Max: " + qtyMax) 
-// console.log("Median: " + qtyMedian)
-// console.log("Min: " + qtyMin)
+    console.log("Remaining t-shirts after " + playerInfo[0].name +  " has bought : "+ last) 
 
 
+  // Total Spent and Cash Left // 
+  playerInfo[0].total = p1qtyBought * playerInfo[0].rate
+  playerInfo[0].cashHeld = 30 - playerInfo[0].total 
+  console.log(playerInfo[0].name + " has spent $" + playerInfo[0].total + " and has left $" + playerInfo[0].cashHeld )
 
-//assign left1 to player // to fix this 
-// let bing1 = d - qtyMax  // 18-7
-// if (left1 == qtyMax) // 
-// console.log( playerInfo[2].name + ' has bought ' + qtyMax + " t-shirts")
-//assign left 2 to player with middle qty//
-//  let bing2 = left1 - qtyMedian
-//  console.log( playerInfo[1].name + ' has bought ' + qtyMedian + " t-shirts") // 5
- //assign last qty to player with leftover qty//
-//  let lasted = left2 - qtyMin
-//  console.log( playerInfo[0].name + ' has bought ' + qtyMin + " t-shirts")
+  playerInfo[1].total = p2qtyBought * playerInfo[1].rate
+  playerInfo[1].cashHeld = 30 - playerInfo[1].total 
+  console.log(playerInfo[1].name + " has spent $" + playerInfo[1].total + " and has left $" + playerInfo[1].cashHeld )
 
-/***************PLAYER  */   
+  playerInfo[2].total = p3qtyBought * playerInfo[2].rate
+  playerInfo[2].cashHeld = 30 - playerInfo[2].total 
+  console.log(playerInfo[2].name + " has spent $" + playerInfo[2].total + " and has left $" + playerInfo[2].cashHeld )
 
- 
+/*************** PLAYER ***********/   
+
+ //===============================//
+//===========SUPPLY PHASE========//
+//===============================//
